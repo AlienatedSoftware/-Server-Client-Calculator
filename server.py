487cbac16d28a5a97308f144601fc89dfa33f10a
AlienatedSoftware <aidancgart@gmail.com>
2021-12-21 00:43:55 +0000

    Socket set up

--- server.py
+++ server.py
@@ -3,4 +3,7 @@ import threading
 
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-print(SERVER)
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
